45bb8477bca37b4da5b80e17107149c15580aa66
Christopher Yuh <christopheryuh9@gmail.com>
2022-03-08 10:16:28 -0500

    model loading done, total num of batches determined

--- models/linear.py
+++ models/linear.py
@@ -1,2 +1,34 @@
-import tensorflow as tf
-from tensorflow.keras.layers import Flatten, Dense, Conv2D
+import torch
+
+from torch.nn import Sequential, Linear, ReLU, Softmax,Flatten
+from torchsummary import summary
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")#use GPU if available
+
+model = Sequential(
+    *[
+    Flatten(),
+    Linear(28*28,14*28),
+    ReLU(),
+    Linear(14*28,14*28),
+    ReLU(),
+    Linear(14*28,14*14),
+    ReLU(),
+    Linear(14*14,7*7),
+    ReLU(),
+    Linear(7*7,49),
+    ReLU(),
+    Linear(49,20),
+    ReLU(),
+    Linear(20,10),
+    ReLU(),
+    Linear(10,10),
+    Softmax()
+    ]
+).to(device)
+
+print(model)
+summary(model,(1,28,28,1))
+
+torch.save(model,"models/linear.pt")
--- train.py
+++ train.py
@@ -1,26 +1,30 @@
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-
 import torch
 
+from torchvision import datasets
+from torchvision.transforms import ToTensor, CenterCrop, Grayscale
+from torchsummary import summary
+
 import argparse
+
+
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")#use GPU if available
 
 parser = argparse.ArgumentParser(description="Experiment Perameters")
 parser.add_argument('training_samples', metavar='N', type=int)
+parser.add_argument('batch_size', metavar='N', type=int)
 parser.add_argument('model_path', metavar='N', type=str)
 parser.add_argument('dataset', metavar='N', type=str)
 
 args = parser.parse_args()
 
 training_samples = args.training_samples
+batch_size = args.batch_size
 model_path = args.model_path
 dataset_name = args.dataset
 
-
-#model = torch.load(model_path)
+model = torch.load(model_path)
 
 
 #load dataset
@@ -28,31 +32,22 @@ dataset_name = args.dataset
 if dataset_name not in ["CIFAR10","MNIST","FashionMNIST"]: #TODO: Finalize datasets
     raise RuntimeError("Unsupported Dataset") #make sure we have a supported datset
 
-    print(train_data.shape)
 #load CIFAR10
 if dataset_name == "CIFAR10":
     train_data = datasets.CIFAR10(
     root="data",
     train=True,
     download=True,
-    transform=ToTensor()
+    transform=[CenterCrop(28),Grayscale(),ToTensor()]
     ) #load train data from torchvision
 
     test_data = datasets.CIFAR10(
         root="data",
         train=False,
         download=True,
-        transform=ToTensor()
+        transform=[CenterCrop(28),Grayscale(),ToTensor()]
     ) #load test data from torchvision
 
-    print("train_data:", train_data,"\n")
-    print("test_data:", test_data,"\n")
-
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=False)
-
-    print("Train_Loader Iters:",len(train_loader),"\n")
-    print("Test_Loader Iters:",len(test_loader),"\n")
 
 
 #load FashionMNIST
@@ -71,14 +66,6 @@ if dataset_name == "FashionMNIST":
         transform=ToTensor()
     ) #load test data from torchvision
 
-    print("train_data:", train_data,"\n")
-    print("test_data:", test_data,"\n")
-
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=False)
-
-    print("Train_Loader Iters:",len(train_loader),"\n")
-    print("Test_Loader Iters:",len(test_loader),"\n")
 
 
 
@@ -98,11 +85,16 @@ if dataset_name == "MNIST":
         transform=ToTensor()
     ) #load test data from torchvision
 
-    print("train_data:", train_data,"\n")
-    print("test_data:", test_data,"\n")
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=False)
 
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=False)
+print("train_data:", train_data,"\n")
+print("test_data:", test_data,"\n")
 
-    print("Train_Loader Iters:",len(train_loader),"\n")
-    print("Test_Loader Iters:",len(test_loader),"\n")
+print("Train_Loader Iters:",len(train_loader),"\n")
+print("Test_Loader Iters:",len(test_loader),"\n")
+
+total_batches = int(training_samples/batch_size)
+print("Total Batches:", total_batches)
+
+summary(model, (1,28,28,1))
